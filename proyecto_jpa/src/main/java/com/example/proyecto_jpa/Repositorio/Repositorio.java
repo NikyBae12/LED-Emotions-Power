5bec7900e1854e909ef35de4840e7aae74a507ee
Omar-Diaz123456 <odiaztauta@gmail.com>
2023-06-26 08:02:44 -0500

    Actualizacion-Dos

--- proyecto_jpa/src/main/java/com/example/proyecto_jpa/Repositorio/Repositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyecto_jpa.Repositorio;
-
-import com.example.proyecto_jpa.Entidad.conexiones;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Repositorio extends JpaRepository<conexiones, Integer> {
-}
